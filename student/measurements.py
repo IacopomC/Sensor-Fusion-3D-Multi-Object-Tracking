3ae557e01fb32ab229e2bdcd2e14cc535e867056
iacopo.catalano <iacopomcatalano@gmail.com>
2021-11-25 13:01:07 +0100

    implement matrix association construction

--- student/measurements.py
+++ student/measurements.py
@@ -12,6 +12,7 @@
 
 # imports
 import numpy as np
+import misc.params as params
 
 # add project directory to python path to enable relative imports
 import os
@@ -19,27 +20,32 @@ import sys
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
-import misc.params as params 
+
 
 class Sensor:
-    '''Sensor class including measurement matrix'''
+    """
+    Sensor class including measurement matrix
+    """
+
     def __init__(self, name, calib):
         self.name = name
         if name == 'lidar':
             self.dim_meas = 3
-            self.sens_to_veh = np.matrix(np.identity((4))) # transformation sensor to vehicle coordinates equals identity matrix because lidar detections are already in vehicle coordinates
-            self.fov = [-np.pi/2, np.pi/2] # angle of field of view in radians
+            # transformation sensor to vehicle coordinates
+            # identity matrix because lidar detections are already in vehicle coordinates
+            self.sens_to_veh = np.matrix(np.identity(4))
+            self.fov = [-np.pi/2, np.pi/2]  # angle of field of view in radians
         
         elif name == 'camera':
             self.dim_meas = 2
-            self.sens_to_veh = np.matrix(calib.extrinsic.transform).reshape(4,4) # transformation sensor to vehicle coordinates
-            self.f_i = calib.intrinsic[0] # focal length i-coordinate
-            self.f_j = calib.intrinsic[1] # focal length j-coordinate
-            self.c_i = calib.intrinsic[2] # principal point i-coordinate
-            self.c_j = calib.intrinsic[3] # principal point j-coordinate
-            self.fov = [-0.35, 0.35] # angle of field of view in radians, inaccurate boundary region was removed
+            self.sens_to_veh = np.matrix(calib.extrinsic.transform).reshape(4, 4)  # transformation sensor to vehicle coordinates
+            self.f_i = calib.intrinsic[0]  # focal length i-coordinate
+            self.f_j = calib.intrinsic[1]  # focal length j-coordinate
+            self.c_i = calib.intrinsic[2]  # principal point i-coordinate
+            self.c_j = calib.intrinsic[3]  # principal point j-coordinate
+            self.fov = [-0.35, 0.35]  # angle of field of view in radians, inaccurate boundary region was removed
             
-        self.veh_to_sens = np.linalg.inv(self.sens_to_veh) # transformation vehicle to sensor coordinates
+        self.veh_to_sens = np.linalg.inv(self.sens_to_veh)  # transformation vehicle to sensor coordinates
     
     def in_fov(self, x):
         # check if an object x can be seen by this sensor
@@ -128,28 +134,30 @@ class Sensor:
 ################### 
         
 class Measurement:
-    '''Measurement class including measurement values, covariance, timestamp, sensor'''
+    """
+    Measurement class including measurement values, covariance, timestamp, sensor
+    """
     def __init__(self, num_frame, z, sensor):
         # create measurement object
-        self.t = (num_frame - 1) * params.dt # time
-        self.sensor = sensor # sensor that generated this measurement
+        self.t = (num_frame - 1) * params.dt  # time
+        self.sensor = sensor  # sensor that generated this measurement
         
         if sensor.name == 'lidar':
-            sigma_lidar_x = params.sigma_lidar_x # load params
+            sigma_lidar_x = params.sigma_lidar_x  # load params
             sigma_lidar_y = params.sigma_lidar_y
             sigma_lidar_z = params.sigma_lidar_z
-            self.z = np.zeros((sensor.dim_meas,1)) # measurement vector
+            self.z = np.zeros((sensor.dim_meas, 1))  # measurement vector
             self.z[0] = z[0]
             self.z[1] = z[1]
             self.z[2] = z[2]
-            self.R = np.matrix([[sigma_lidar_x**2, 0, 0], # measurement noise covariance matrix
+            self.R = np.matrix([[sigma_lidar_x**2, 0, 0],  # measurement noise covariance matrix
                                 [0, sigma_lidar_y**2, 0], 
                                 [0, 0, sigma_lidar_z**2]])
-            
+            self.height = z[3]
             self.width = z[4]
             self.length = z[5]
-            self.height = z[3]
             self.yaw = z[6]
+
         elif sensor.name == 'camera':
             
             ############
